9826d3171bdeb574e114215e77f76ffe44387c52
Sam Gammon <sam@elide.dev>
2025-05-31 21:57:51 -0700

    chore: use new apply-from endpoint
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- example-project-remote/settings.gradle.kts
+++ example-project-remote/settings.gradle.kts
@@ -1,3 +1,4 @@
 // Use `latest` for the latest version, or any other tag, branch, or commit SHA on this project.
 val elidePluginVersion: String by settings
-apply(from = "https://gradle.elide.dev/$elidePluginVersion/elide.gradle.kts")
+apply(from = "https://gradle.elide.dev/tag/$elidePluginVersion/elide.gradle.kts")
+
